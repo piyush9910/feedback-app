7e9408b5ad9269a9f2a1e3bf27e0185964b2174a
piyush9910 <suripiyush4@gmail.com>
2024-09-16 23:09:14 +0530

    Initial Commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,13 @@
+const FeedbackData = [
+  { id: 1, rating: 4.5, text: 'Great app, very user-friendly!' },
+  {
+    id: 2,
+    rating: 3.8,
+    text: 'Good features, but needs improvement in speed.',
+  },
+  { id: 3, rating: 5.0, text: 'Excellent! Exceeded my expectations.' },
+  { id: 4, rating: 2.5, text: 'Not bad, but has many bugs.' },
+  { id: 5, rating: 4.2, text: 'Solid app, will recommend to others.' },
+];
+
+export default FeedbackData;
